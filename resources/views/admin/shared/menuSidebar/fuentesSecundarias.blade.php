8be3c554ff5bffd2581f615a12a8d08579606229
Unknown <analiz14_q@hotmail.com>
2018-06-18 19:09:29 -0500

    Menu Fuentes Secundarias

--- resources/views/admin/shared/menuSidebar/fuentesSecundarias.blade.php
+++ resources/views/admin/shared/menuSidebar/fuentesSecundarias.blade.php
@@ -1,18 +1,6 @@
-@@ -1,13 +1,31 @@
+
 <li><a><i class="fa fa-file-text-o"></i> Fuentes Secundarias <span class="fa fa-chevron-down"></span></a> 
-    <ul class="nav child_menu"> 
-        <li><a href="general_elements.html">General Elements</a></li> 
-        <li><a href="media_gallery.html">Media Gallery</a></li> 
-        <li><a href="typography.html">Typography</a></li> 
-        <li><a href="icons.html">Icons</a></li> 
-        <li><a href="glyphicons.html">Glyphicons</a></li> 
-        <li><a href="widgets.html">Widgets</a></li> 
-        <li><a href="invoice.html">Invoice</a></li> 
-        <li><a href="inbox.html">Inbox</a></li> 
-        <li><a href="calendar.html">Calendar</a></li> 
-    </ul> 
-</li>
- \ No newline at end of file 
+
 <ul class="nav child_menu"> 
         <li><a href="#level2_1"><i class="fa fa-suitcase"></i>Dependencias</a> 
         </li> 
