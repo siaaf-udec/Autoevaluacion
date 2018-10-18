bd06b5df625807ac0fd6899d6d71cab52a444c0c
Unknown <andres950523@hotmail.com>
2018-10-18 11:29:12 -0500

    Correccion

--- resources/views/autoevaluacion/SuperAdministrador/ActividadesMejoramiento/index.blade.php
+++ resources/views/autoevaluacion/SuperAdministrador/ActividadesMejoramiento/index.blade.php
@@ -1,5 +1,50 @@
-
-            
+{{-- Titulo de la pagina --}}
+@section('title', 'Actividades de Mejoramiento')
+ {{-- Contenido principal --}}
+@extends('admin.layouts.app')
+@section('content')
+    @component('admin.components.panel')
+        @slot('title', 'Actividades de Mejoramiento')
+        @if(session()->get('id_proceso'))
+            @if(isset($planMejoramiento))
+                @can('VER_ACTIVIDADES_MEJORAMIENTO')
+                    <div class="col-md-12">
+                        @component('admin.components.datatable', ['id' => 'actividades_mejoramiento_table_ajax']) @slot('columns', [
+            'id','Factor','Caracteristica','Nombre', 'Descripcion', 'Fecha de Inicio', 'Fecha de Finalizacion', 'Responsable',
+            'Acciones' => ['style' => 'width:85px;']]) @endcomponent
+                    </div>
+                @endcan
+            @else
+                Este proceso aun no tiene plan de mejoramiento.
+            @endif
+        @else
+            Por favor seleccione un proceso
+        @endif
+    @endcomponent
+@endsection
+{{-- Scripts necesarios para el formulario --}}
+@push('scripts')
+    <!-- Datatables -->
+    <script src="{{asset('gentella/vendors/DataTables/datatables.min.js') }}"></script>
+    <script src="{{asset('gentella/vendors/sweetalert/sweetalert2.all.min.js') }}"></script>
+    <!-- PNotify -->
+    <script src="{{ asset('gentella/vendors/pnotify/dist/pnotify.js') }}"></script>
+    <script src="{{ asset('gentella/vendors/pnotify/dist/pnotify.buttons.js') }}"></script>
+    <script src="{{ asset('gentella/vendors/pnotify/dist/pnotify.nonblock.js') }}"></script>
+ @endpush
+{{-- Estilos necesarios para el formulario --}}
+@push('styles')
+    <!-- Datatables -->
+    <link href="{{ asset('gentella/vendors/DataTables/datatables.min.css') }}" rel="stylesheet">
+    <!-- PNotify -->
+    <link href="{{ asset('gentella/vendors/pnotify/dist/pnotify.css') }}" rel="stylesheet">
+    <link href="{{ asset('gentella/vendors/pnotify/dist/pnotify.buttons.css') }}" rel="stylesheet">
+    <link href="{{ asset('gentella/vendors/pnotify/dist/pnotify.nonblock.css') }}" rel="stylesheet">
+ @endpush
+{{-- Funciones necesarias por el formulario --}}
+@push('functions')
+    <script type="text/javascript">
+        $(document).ready(function () {
             @if(session()->get('id_proceso'))
                 let sesion = sessionStorage.getItem("update");
                 if (sesion != null) {
@@ -44,3 +89,88 @@
                                 responsivePriority: 2
                         }
                     ],
+                    language: {
+                    "sProcessing": "Procesando...",
+                    "sLengthMenu": "Mostrar _MENU_ registros",
+                    "sZeroRecords": "No se encontraron resultados",
+                    "sEmptyTable": "Ningún dato disponible en esta tabla",
+                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix": "",
+                    "sSearch": "Buscar:",
+                    "sUrl": "",
+                    "sInfoThousands": ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst": "Primero",
+                        "sLast": "Último",
+                        "sNext": "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    }
+                },
+             });
+             table.on('click', '.remove', function (e) {
+                e.preventDefault();
+                $tr = $(this).closest('tr');
+                var dataTable = table.row($tr).data();
+                var route = '{{ url('admin/actividades_mejoramiento') }}' + '/' + dataTable.PK_ACM_Id;
+                var type = 'DELETE';
+                dataType: "JSON",
+                    SwalDelete(dataTable.PK_ASP_Id, route);
+             });
+            table.on('click', '.edit', function (e) {
+                e.preventDefault();
+                $tr = $(this).closest('tr');
+                var dataTable = table.row($tr).data();
+                var route = '{{ url('admin/actividades_mejoramiento/') }}' + '/' + dataTable.PK_ACM_Id + '/edit';
+                window.location.href = route;
+             });
+            @endif
+        });
+         function SwalDelete(id, route) {
+            swal({
+                title: 'Esta seguro?',
+                text: "La actividad de mejoramiento sera eliminada permanentemente!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminar!',
+                showLoaderOnConfirm: true,
+                cancelButtonText: "Cancelar",
+                preConfirm: function () {
+                    return new Promise(function (resolve) {
+                         $.ajax({
+                            type: 'DELETE',
+                            url: route,
+                            data: {
+                                '_token': $('meta[name="_token"]').attr('content'),
+                            },
+                            success: function (response, NULL, jqXHR) {
+                                table.ajax.reload();
+                                new PNotify({
+                                    title: response.title,
+                                    text: response.msg,
+                                    type: 'success',
+                                    styling: 'bootstrap3'
+                                });
+                            }
+                        })
+                            .done(function (response) {
+                                swal('Eliminada exitosamente!', response.message, response.status);
+                            })
+                            .fail(function () {
+                                swal('Oops...', 'Something went wrong with ajax !', 'error');
+                            });
+                    });
+                },
+                allowOutsideClick: false
+            });
+         }
+     </script>
+@endpush 
